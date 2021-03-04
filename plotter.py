d944ce0d9d79afbbdd45e0443eddc6b2a8c97b64
Monika Scholz <mscholz@caesar.de>
2021-03-04 17:01:28 +0100

    reversal detection works

--- plotter.py
+++ plotter.py
@@ -5,8 +5,9 @@ import matplotlib.pylab as plt
 from pharaglow import io, extract
 
 class Worm:
+    """class to contain data from a single pharaglow result."""
     def __init__(self, filename, columns,fps, scale, **kwargs):
-        """load a pharaglow results file."""
+        """initialize object and load a pharaglow results file."""
         self.flag = False
         print('Reading', filename)
         # keep some metadata
@@ -17,6 +18,7 @@ class Worm:
 
 
     def _load(self, filename, columns, fps, scale, **kwargs):
+        """load data."""
         traj = io.load(filename, orient='split')
         # drop all columns except the ones we want
         traj = traj.filter(columns)
@@ -42,6 +44,10 @@ class Worm:
             self.traj = []
         self.data = traj
     
+
+    def __repr__(self):
+        return f"Worm \n with underlying data: {self.data.describe()}"
+
 
     def __len__(self):
         return len(self.data)
@@ -70,30 +76,65 @@ class Worm:
     
 
     def get_data(self, key = None):
+        """return a column of data or the whole pandas dataframe."""
         if key == None:
             return self.data
         else:
             assert key in self.data.columns, f'The key {key} does not exist in the data.'
             return self.data[key]
     
-    def calculate_reversals(self, angle_treshold):
-        pass
 
-
+    def calculate_reversals(self, animal_size, angle_treshold):
+        """Adaptation of the Hardaker's method to detect reversal event. 
+        A single worm's centroid trajectory is re-sampled with a distance interval equivalent to 1/10 
+        of the worm's length (100um) and then reversals are calculated from turning angles.
+        Inputs:
+            animal_size: animal size in um.
+            angle_threshold (degree): what defines a turn 
+        Output: None, but adds a column 'reversals' to  self.data.
+        """
+        # resampling
+        # Calculate the distance cover by the centroid of the worm between two frames um
+        distance = np.cumsum(self.data['velocity'])*self.data['time'].diff()
+        # find the maximum number of worm lengths we have travelled
+        maxlen = distance.max()/animal_size
+        # make list of levels that are multiples of animal size
+        levels = np.arange(animal_size, maxlen*animal_size, animal_size)
+        # Find the indices where the distance is equal or the closest to the pixel interval by repeatedly subtracting the levels
+        indices = []
+        for level in levels:
+            idx = distance.sub(level).abs().idxmin()
+            indices.append(idx)
+        # create a downsampled trajectory from these indices
+        traj_Resampled = self.data.loc[indices, ['x', 'y']].diff()
+        # we ignore the index here for the shifted data
+        traj_Resampled[['x1', 'y1']] = traj_Resampled.shift(1).fillna(0)
+        # use the dot product to calculate the andle
+        def angle(row):
+            v1 = [row.x, row.y]
+            v2 = [row.x1, row.y1]
+            return np.degrees(np.arccos(np.dot(v1, v2)/np.linalg.norm(v1)/np.linalg.norm(v2)))
+        traj_Resampled['angle'] = traj_Resampled.apply(lambda row: angle(row), axis =1)
+        rev = traj_Resampled.index[traj_Resampled.angle>=angle_treshold]
+        self.data['reversals'] = 0
+        self.data['reversals'].loc[rev] = 1
+        
 
 class Experiment:
     """Wrapper class which is a container for individual worms."""
     # class attributes
-    def __init__(self, strain, condition, scale, fps, samples = None):
+    def __init__(self, strain, condition, scale, fps, samples = None, color = None):
         self.strain = strain
         self.condition = condition
+        self.scale = scale
+        self.fps = fps
+
         if samples == None:
             self.samples = []
         else:
             self.samples = samples[:]
-        self.scale = scale
-        self.fps = fps
-
+        self.color = color
+        
     
     def __repr__(self):
         return f"Experiment \n Strain: {self.strain},\n Condition: {self.condition}\n N = {len(self.samples)}."
@@ -159,6 +200,11 @@ class Experiment:
     #   get/set attributes
     #
     #######################################
+    def set_color(self, color):
+        """sets the color used for plotting this experiment. Can be a defined color string or hex-code.
+            Anything that matlab understands is valid.
+        """
+
     def get_sample(self, N):
         """get a sample of the experiment.
             Returns a Worm() object
@@ -194,7 +240,8 @@ class Experiment:
     def plot_trajectories(self, ax):
         """plot all x-y trajectories."""
         for worm in self.samples:
-            ax.plot(worm.data.x, worm.data.y)
+            ax.plot(worm.data.x, worm.data.y, color = None)
+
 
     def plot_timeseries(self, key, ax, average = True):
         """plot a property as a function of time.
@@ -213,22 +260,45 @@ class Experiment:
         pass
     
 
-""" Example code loading an experiment."""
+######################################
+#
+#    Example code loading an experiment
+#
+#######################################
+
 control = Experiment(strain='GRU101', condition='Entry', scale=2.34, fps = 30.)
 control.load_data('/home/mscholz/Desktop/TestOutput_MS0006', nmax = 2)
-# get one worm
-a = control[0:2]
-print(a.get_sample_metric('velocity', 'N'))
-for i, metric in enumerate(['mean', 'sem', 'std', 'N']):
-    plt.subplot(2,2,i+1)
-    t = a.get_sample_metric('velocity', metric)
-    plt.plot(t)
-plt.show()
 
-plt.show()
-a.plot_trajectories(plt.gca())
-plt.show()
+######################################
+#
+#    class supports slicing
+#
+#######################################
+# get just a few samples. a is still an Experiment object
+a = control[0:2]
+# if you want the worm class, use get_sample(n) 
 w = control.get_sample(0)
+
+######################################
+#
+#    calculate metrics at the worm level or experiment level
+#
+#######################################
+
+# for i, metric in enumerate(['mean', 'sem', 'std', 'N']):
+#     plt.subplot(2,2,i+1)
+#     t = a.get_sample_metric('velocity', metric)
+#     plt.plot(t)
+# plt.show()
+
+# plt.show()
+# a.plot_trajectories(plt.gca())
+# plt.show()
+w = control.get_sample(0)
+w.calculate_reversals(animal_size=50, angle_treshold=120)
+w.get_data('reversals').plot()
+plt.show()
+print(w)
 for i in ['mean', 'sem', 'std', 'N']:
     t = w.get_metric('velocity', i)
     print(t)
